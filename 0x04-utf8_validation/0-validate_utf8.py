116d90b8936ed6eff9f0dae0b29ee85628c511e2
joehsn <joehsn@outlook.com>
2024-10-28 20:10:58 +0300

    0. UTF-8 Validation task is done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0. UTF-8 Validation task's module.
+"""
+
+
+def validUTF8(data):
+    """
+    A method that determines if a given
+    data set represents a valid UTF-8 encoding.
+    """
+    remaining_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if remaining_bytes == 0:
+            if byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            elif byte >> 7 == 0:
+                continue
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            remaining_bytes -= 1
+
+    return remaining_bytes == 0
