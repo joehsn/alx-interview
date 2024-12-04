9d7f4f0d27e026185dc4104bf38dd58e6f838b72
joehsn <joehsn@outlook.com>
2024-12-04 19:01:06 +0200

    0x09. Island Perimeter task: done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+""" 0. Island Perimeter module.
+"""
+
+
+def island_perimeter(grid) -> int:
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of int): 2D grid where 0 represents
+        water and 1 represents land.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:  # Land cell
+                # Check all four sides
+                # Top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Bottom
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Right
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
